49d970e2bd7f376ebb1974d1df7ace0fbafe577a
biacattaneo <biacattaneo2013@gmail.com>
2024-10-04 17:21:35 -0300

    feat: add edit page

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -3,7 +3,7 @@ import { ProductsService } from './../../shared/services/products.service';
 import { Component, inject } from '@angular/core';
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -17,10 +17,15 @@ export class ListComponent {
   products: Product[] = [];
 
   productsService = inject(ProductsService);
+  router = inject(Router);
 
   ngOnInit(){
     this.productsService.getAll().subscribe((products) => {
       this.products = products;
     });
+  }
+
+  onEdit(){
+    this.router.navigateByUrl('/edit-product');
   }
 }
