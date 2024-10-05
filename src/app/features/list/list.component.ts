d45ac1effb894cdd05159c86042fbbabef0dc426
biacattaneo <biacattaneo2013@gmail.com>
2024-10-04 21:49:58 -0300

    feat: create delete

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,10 +1,31 @@
 import { ProductsService } from './../../shared/services/products.service';
 // import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
 import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  template: `
+  <h2 mat-dialog-title>Delete file</h2>
+<mat-dialog-content>
+  Would you like to delete cat.jpeg?
+</mat-dialog-content>
+<mat-dialog-actions>
+  <button mat-button mat-dialog-close>No</button>
+  <button mat-button mat-dialog-close cdkFocusInitial>Ok</button>
+</mat-dialog-actions>
+`,
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ConfirmationDialogComponent {
+  readonly dialogRef = inject(MatDialogRef<ConfirmationDialogComponent>);
+}
 
 @Component({
   selector: 'app-list',
@@ -14,18 +35,25 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+
   products: Product[] = [];
 
   productsService = inject(ProductsService);
   router = inject(Router);
 
-  ngOnInit(){
+  matDialog = inject(MatDialog);
+
+  ngOnInit() {
     this.productsService.getAll().subscribe((products) => {
       this.products = products;
     });
   }
 
-  onEdit(product: Product){
+  onEdit(product: Product) {
     this.router.navigate(['/edit-product', product.id]);
+  }
+
+  onDelete(product: Product) {
+    this.matDialog.open(ConfirmationDialogComponent).afterClosed().subscribe((data) => {console.log(data)});
   }
 }
